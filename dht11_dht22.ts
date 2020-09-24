862e825447b2dbae66f1d1abd053fe8468b873d2
lz <328222051@qq.com>
2020-09-24 14:10:44 +0800

    初始工程

---
+++ dht11_dht22.ts
@@ -0,0 +1,120 @@
+/**
+ * MakeCode editor extension for DHT11 and DHT22 humidity/temperature sensors
+ * http://www.micropython.org.cn
+ */
+
+enum DHTtype {
+    //% block="DHT11"
+    DHT11,
+    //% block="DHT22"
+    DHT22,
+}
+
+enum dataType {
+    //% block="温度"
+    temperature,
+    //% block="湿度"
+    humidity,
+}
+
+//% weight=100 color=#ff8f3f icon="\uf043" block="温湿度传感器"
+namespace dht11{
+
+    let _temperature: number = -999.0
+    let _humidity: number = -999.0
+    let _readSuccessful: boolean = false
+
+    /**
+    * 读取温湿度数据
+    */
+    //% blockId="query_Data" block="温湿度传感器设置类型 $DHT 通讯引脚 $dataPin 开启上拉电阻 $pullUp"
+    //% weight=70 blockGap=8
+    //% pullUp.defl=true
+    export function queryData(DHT: DHTtype, dataPin: DigitalPin, pullUp: boolean) {
+
+        //initialize
+        let startTime: number = 0
+        let endTime: number = 0
+        let checksum: number = 0
+        let checksumTmp: number = 0
+        let dataArray: boolean[] = []
+        let resultArray: number[] = []
+        for (let index = 0; index < 40; index++) dataArray.push(false)
+        for (let index = 0; index < 5; index++) resultArray.push(0)
+        _humidity = -999.0
+        _temperature = -999.0
+        _readSuccessful = false
+
+        startTime = input.runningTimeMicros()
+
+        //request data
+        pins.digitalWritePin(dataPin, 0) //begin protocol
+        basic.pause(18)
+        if (pullUp) pins.setPull(dataPin, PinPullMode.PullUp) //pull up data pin if needed
+        pins.digitalReadPin(dataPin)
+        control.waitMicros(20)
+        while (pins.digitalReadPin(dataPin) == 1);
+        while (pins.digitalReadPin(dataPin) == 0); //sensor response
+        while (pins.digitalReadPin(dataPin) == 1); //sensor response
+
+        //read data (5 bytes)
+        for (let index = 0; index < 40; index++) {
+            while (pins.digitalReadPin(dataPin) == 1);
+            while (pins.digitalReadPin(dataPin) == 0);
+            control.waitMicros(28)
+            //if sensor pull up data pin for more than 28 us it means 1, otherwise 0
+            if (pins.digitalReadPin(dataPin) == 1) dataArray[index] = true
+        }
+
+        endTime = input.runningTimeMicros()
+
+        //convert byte number array to integer
+        for (let index = 0; index < 5; index++)
+            for (let index2 = 0; index2 < 8; index2++)
+                if (dataArray[8 * index + index2]) resultArray[index] += 2 ** (7 - index2)
+
+        //verify checksum
+        checksumTmp = resultArray[0] + resultArray[1] + resultArray[2] + resultArray[3]
+        checksum = resultArray[4]
+        if (checksumTmp >= 512) checksumTmp -= 512
+        if (checksumTmp >= 256) checksumTmp -= 256
+        if (checksum == checksumTmp) _readSuccessful = true
+
+        //read data if checksum ok
+        if (_readSuccessful) {
+            if (DHT == DHTtype.DHT11) {
+                //DHT11
+                _humidity = resultArray[0] + resultArray[1] / 100
+                _temperature = resultArray[2] + resultArray[3] / 100
+            } else {
+                //DHT22
+                let temp_sign: number = 1
+                if (resultArray[2] >= 128) {
+                    resultArray[2] -= 128
+                    temp_sign = -1
+                }
+                _humidity = (resultArray[0] * 256 + resultArray[1]) / 10
+                _temperature = (resultArray[2] * 256 + resultArray[3]) / 10 * temp_sign
+            }
+        }
+
+    }
+
+    /**
+    *  获取温湿度值
+    */
+    //% block="获取 $data 值"
+    export function readData(data: dataType): number {
+        return data == dataType.humidity ? _humidity : _temperature
+    }
+
+    /**
+    * 判断是否读取成功
+    */
+    //% block="温湿度数据读取成功"
+    export function readDataSuccessful(): boolean {
+        return _readSuccessful
+    }
+
+}
+
